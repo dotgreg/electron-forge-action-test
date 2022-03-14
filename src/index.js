cec84c681394121bb4e3b22fa3b5027a46db70bd
greg@1m <greg@1m>
2022-03-14 17:54:37 +0100

    test ci

--- src/index.js
+++ src/index.js
@@ -29,18 +29,35 @@ const logWrite = (content) => {
 		})
 }
 
+const execCmdLog = (cmd, params) => {
+		const spawn = require( 'child_process' ).spawn;
+		try {
+				const child = spawn( cmd, params);  
+				child.stdout.on( 'data', data => {
+						data = `${data}`.substring(1,400);
+						const str = `[${cmd}] ${new Date().getTime()}: ${data}`;
+						console.log( str );
+						logWrite(str);  
+				});
+				child.stderr.on( 'data', data => {
+						data = `${data}`.substring(1,400);
+						const str = `[${cmd} ERROR!] ${new Date().getTime()}: ${data}`;
+						console.log( str );
+						logWrite(str);  
+				});
+		} catch (e) {
+				logWrite(`[${cmd} ERROR JS!] ${new Date().getTime()} : ${e}`);
+		}
+}
+
 const rgBinTest = () => {
 		const appRootDir = require('app-root-dir').get();
 		const binPath = appRootDir + '/bin/rg';
 		console.log({binPath});
-		const spawn = require( 'child_process' ).spawn;
-		const child = spawn( binPath, ['woop', `/Users/gregoirethiebault/Desktop/_dev/data-test`]);  //add whatever switches you need here, test on command line first
-		child.stdout.on( 'data', data => {
-				console.log( `stdout: ${data}` );
-				logWrite(`${data}`);
-		});
-		child.stderr.on( 'data', data => {
-				console.log( `stderr: ${data}` );
-				logWrite(`${data}`);
-		});
+		//const child = spawn( binPath, ['woop', `/Users/gregoirethiebault/Desktop/_dev/data-test`]);  //add whatever switches you need here, test on command line first
+		//execCmdLog(binPath, ['woop', `/Users/gregoirethiebault/Desktop/_dev/data-test`]);
+		execCmdLog('pwd', []);
+		//execCmdLog(binPath, ['woop', `/Users/gregoirethiebault/Desktop/_dev/data-test`]);
+		execCmdLog(binPath, ['woop', `~/test-data-folder`]);
+		//execCmdLog('ls', ['*']);
 };
